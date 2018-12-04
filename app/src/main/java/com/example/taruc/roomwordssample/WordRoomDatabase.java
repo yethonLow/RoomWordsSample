facf7211e0933885da7b161670540cd0ea3995ad
lowyh-ja16@student.tarc.edu.my <yethon1997>
2018-12-04 11:33:14 +0800

    update Week 8 RoomShare

--- app/src/main/java/com/example/taruc/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/taruc/roomwordssample/WordRoomDatabase.java
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
@@ -30,5 +31,37 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
 
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+    private final WordDao mDao;
+    String[] words = {"dolphin", "crocodile", "cobra"};
+
+    PopulateDbAsync(WordRoomDatabase db) {
+        mDao = db.wordDao();
+    }
+
+    @Override
+    protected Void doInBackground(final Void... params) {
+        // Start the app with a clean database every time.
+        // Not needed if you only populate the database
+        // when it is first created
+        mDao.deleteAll();
+
+        for (int i = 0; i <= words.length - 1; i++) {
+            Word word = new Word(words[i]);
+            mDao.insert(word);
+        }
+        return null;
+    }
+}
 }
